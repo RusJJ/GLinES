406140b60679c1f42fe6426c210ed0b1bd8b503f
[-=KILL MAN=-] <rusjj@yandex.ru>
2025-08-13 04:39:09 +0300

    Update buffer.cpp

--- src/gl/buffer.cpp
+++ src/gl/buffer.cpp
@@ -2,10 +2,15 @@
 
 void* WRAP(glMapBuffer(GLenum target, GLenum access))
 {
-    return glMapBufferRange(target, 0, 4, access);
+    if(access == 0x88B8) access = GL_MAP_READ_BIT;
+    else if(access == 0x88B9) access = GL_MAP_WRITE_BIT;
+    else if(access == 0x88BA) access = GL_MAP_READ_BIT|GL_MAP_WRITE_BIT;
+    // target == GL_QUERY_BUFFER ?
+    return glMapBufferRange(target, 0, sizeof(void*), access);
 }
 
 void WRAP(glDrawBuffer(GLenum buf))
 {
-    return glDrawBuffers(1, &buf);
+    GLenum bbuf = buf;
+    return glDrawBuffers(1, &bbuf);
 }
